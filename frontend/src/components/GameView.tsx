afea8185dfc5b5805ff0f4c7fab4a00911dafed8
Sarah Schwartz <58856580+sarahschwartz@users.noreply.github.com>
2025-08-29 10:56:22 -0600

    feat: add copy to clipboard toast

--- frontend/src/components/GameView.tsx
+++ frontend/src/components/GameView.tsx
@@ -9,7 +9,13 @@ import Spinner from "./Spinner";
 import { useEffect, useState } from "react";
 import { getContractAddress } from "../../utils/wagmi";
 
-export function GameView({ playerAddress, chainId }: { playerAddress: `0x${string}`, chainId: number }) {
+export function GameView({
+  playerAddress,
+  chainId,
+}: {
+  playerAddress: `0x${string}`;
+  chainId: number;
+}) {
   const [update, setUpdate] = useState<number>(0);
   const { writeContract, data: hash, isPending } = useWriteContract();
 
@@ -51,28 +57,65 @@ export function GameView({ playerAddress, chainId }: { playerAddress: `0x${strin
     setUpdate((prev) => prev + 1);
   }, [isConfirmed]);
 
+  const ClipboardIcon = () => {
+  const [show, setShow] = useState(false);
+
+  function copy(){
+    if(hash) navigator.clipboard.writeText(hash);
+    setShow(true);
+    setTimeout(() => { setShow(false) }, 3000);
+  }
+  return (
+  <>
+  <div style={{ height: 20, width: 20, cursor: "pointer" }} onClick={copy}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M9 18q-.825 0-1.412-.587T7 16V4q0-.825.588-1.412T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.587 1.413T18 18zm0-2h9V4H9zm-4 6q-.825 0-1.412-.587T3 20V6h2v14h11v2zm4-6V4z"
+      />
+    </svg>
+  </div>
+  <div id="toast" className={show ? "show" : ""}>Copied tx hash to clipboard</div>
+  </>
+)};
+
   return (
     <div className="card">
-      <button className="buttonLarge" disabled={isPending} onClick={() => incrementScore()}>
+      <button
+        className="buttonLarge"
+        disabled={isPending}
+        onClick={() => incrementScore()}
+      >
         Increment Score
       </button>
       <div style={{ marginTop: "12px" }}>
         {isConfirming && <Spinner />}
         {isConfirmed && hash && (
           <div
-            style={{ cursor: "pointer" }}
-            onClick={() => {
-              navigator.clipboard.writeText(hash);
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "8px",
+              alignItems: "center"
             }}
           >
-            Transaction hash: {hash}
+            <ClipboardIcon />
+            <p>
+              Transaction hash: {hash.substring(0, 8)}...
+              {hash.substring(hash.length - 6)}
+            </p>
           </div>
         )}
       </div>
       <div>
         <h3>Your Stats</h3>
         <p>Your High Score: {playerHighestScore}</p>
-        <p>Game High Score: {highestScore}</p>
+        <p>Game Chain High Score: {highestScore}</p>
       </div>
       <LeaderboardStats update={update} />
     </div>
--- frontend/src/components/LeaderboardStats.tsx
+++ frontend/src/components/LeaderboardStats.tsx
@@ -33,7 +33,7 @@ export function LeaderboardStats({ update }: { update: number}) {
 
   return (
     <div>
-      <h3>Network Leaderboard</h3>
+      <h3>Leaderboard Chain</h3>
       <p>
         Winning Chain: {winningChainName} ({winningChainId})
       </p>
